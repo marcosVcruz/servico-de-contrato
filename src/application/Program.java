e6da6cfc75faaca713d61fe805e1765e456bef5b
Marcos Vinicius <marcos.viniciuscruz00@gmail.com>
2023-07-27 19:41:53 -0300

    processamento de contrato

---
+++ src/application/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import entities.Contract;
+import entities.Installment;
+import services.ContractService;
+import services.PaypalService;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) throws ParseException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Entre os dados do contrato: ");
+        System.out.print("Numero: ");
+        int number = sc.nextInt();
+        System.out.print("Data (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(sc.next(), fmt);
+        System.out.print("Valor do contrato: ");
+        double totalValue = sc.nextDouble();
+
+        Contract obj = new Contract(number, date, totalValue);
+
+        System.out.print("Entre com o numero de parcelas: ");
+        int n = sc.nextInt();
+
+        ContractService service = new ContractService(new PaypalService());
+
+        service.processContract(obj, n);
+
+        System.out.println("PARCELAS: ");
+
+        for(Installment installment : obj.getInstallments()){
+            System.out.println(installment);
+        }
+
+        sc.close();
+    }
+}
